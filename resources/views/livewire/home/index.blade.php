778221ed889d0777a965fca88f950f7038d731ad
MOSETI <enock.g.moseti@gmail.com>
2022-06-10 10:27:22 +0300

    add: home component

---
+++ resources/views/livewire/home/index.blade.php
@@ -0,0 +1,22 @@
+<div>
+    {{-- Be like water. --}}
+    <div>
+        <h1>Fyyur 🔥</h1>
+        <p>Where Musical artists meet musical venues</p>
+        <div>
+            <div>
+                <a href="/venues">Find a venue</a>
+                <a href="/venues/create">Post a venue</a>
+            </div>
+            <div>
+                <a href="artists">Find an artist</a>
+                <a href="artists/create">Post an artist</a>
+            </div>
+        </div>
+        <p>Publicize about your show for free.</p>
+        <a href="/shows/create">Post a show</a>
+    </div>
+    <div>
+        <img src="{{asset('images/band.jpg')}}" alt="" />
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', \App\Http\Livewire\Home\Index::class);
+
 Route::get('/venues', \App\Http\Livewire\Venue\Index::class);
