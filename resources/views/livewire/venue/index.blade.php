ac62f2a015a64415cb32ddaf5fcdf16db9d8c805
MOSETI <enock.g.moseti@gmail.com>
2022-06-10 09:04:36 +0300

    add: show venues component

---
+++ resources/views/livewire/venue/index.blade.php
@@ -0,0 +1,16 @@
+<div>
+    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <div>
+        <h2>San Francisco, CA</h2>
+        <ul>
+            <li>The Musical Hop</li>
+            <li>Park Square Live Music & Coffee</li>
+        </ul>
+    </div>
+    <div>
+    <h2>New York, NY</h2>
+    <ul>
+        <li>The Duelling Pianos Bar</li>
+    </ul>
+    </div>
+</div>
