92789b8561a760a035cde70fa79622ef573611c6
MOSETI <enock.g.moseti@gmail.com>
2022-06-11 11:34:25 +0300

    ~genre & state enum

---
+++ app/Enum/GenresEnum.php
@@ -0,0 +1,26 @@
+<?php
+namespace Enum;
+
+enum Genre: string
+{
+    case Alternative = 'Alternative';
+    case Blues = 'Blues';
+    case Classical = 'Classical';
+    case Country = 'Country';
+    case Electronic = 'Electronic';
+    case Folk = 'Folk';
+    case Funk = 'Funk';
+    case Hip_Hop = 'Hip-Hop';
+    case Heavy_Metal = 'Heavy Metal';
+    case Instrumental = 'Instrumental';
+    case Jazz = 'Jazz';
+    case Musical_Theatre = 'Musical Theatre';
+    case Pop = 'Pop';
+    case Punk = 'Punk';
+    case R_n_B = 'R&B';
+    case Reggae = 'Reggae';
+    case Rock_n_Roll = 'Rock n Roll';
+    case Soul = 'Soul';
+    case Other = 'Other';
+}
+
